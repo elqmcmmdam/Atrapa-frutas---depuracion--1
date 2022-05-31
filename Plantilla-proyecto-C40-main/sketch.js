9ac8a949eb0a4fdd0ae7a1bd9c472391219c4855
elqmcmmdam <98717932+elqmcmmdam@users.noreply.github.com>
2022-05-30 20:44:40 -0500

    Add files via upload

---
+++ Plantilla-proyecto-C40-main/sketch.js
@@ -0,0 +1,55 @@
+//Maestra, cuando ejecuto el programa me aparece, la url de la base de datos en la pantalla pero no se
+//ve el juego, por otro lado, puse en el index.html el codigo que se debe de poner que te da la base de datos,
+//pero solo aparecen letras de color blanco, y no parece que ese codigo tenga efecto en el juego, me gustaría
+//que me pudiera aclarar que está pasando o que hice mal (configuré la base de datos correctamente), gracias.
+
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+var score =0;
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+var fruitGroup;
+var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var player_img;
+var player1score =0;
+var player2score =0;
+
+function preload(){
+  back_img = loadImage("images/jungle.jpg");
+  player_img = loadImage("images/basket2.png");
+  fruit1_img = loadImage("images/apple2.png");
+  fruit2_img = loadImage("images/banana2.png");
+  fruit3_img = loadImage("images/melon2.png");
+  fruit4_img = loadImage("images/orange2.png");
+  fruit5_img = loadImage("images/pineapple2.png");
+  fruitGroup = new Group();
+}
+function setup() {
+  createCanvas(1000, 600);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw() {
+  background(back_img);
+  
+   if (playerCount === 2) {
+     game.update(1);
+   }
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+   }
+   if (gameState === 2) {
+    
+     game.end();
+   }
+}
